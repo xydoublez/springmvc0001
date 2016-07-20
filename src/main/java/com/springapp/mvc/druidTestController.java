6caa8f8e6a2bdbd7e09836d0aed7b0b463556ad9
李志强 <xydoublez@hotmail.com>
2016-07-20 23:37:05 +0800

    将配置自定义 以及添加数据库访问，未成功

---
+++ src/main/java/com/springapp/mvc/druidTestController.java
@@ -0,0 +1,54 @@
+package com.springapp.mvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by Administrator on 2016-07-20.
+ */
+@Controller
+@RequestMapping("/test1")
+public class druidTestController {
+    private static final Logger logger = LoggerFactory.getLogger(druidTestController.class);
+    private JdbcTemplate jdbcTemplate;
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+   public String Get(){
+        String result ="druid is very good";
+        ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext("mvc-dispatcher-servlet.xml");
+        DataSource dataSource = (DataSource)context.getBean("dataSource");
+        /*
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        Map<String,Object> map = jdbcTemplate.queryForMap("SELECT * FROM VIEW_REGLIST WHERE ROWNUM<10;");
+        */
+        Connection con = null;
+        try {
+            con = dataSource.getConnection();
+            Statement st  = con.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM VIEW_REGLIST WHERE ROWNUM<10;");
+            while (rs.next()) {
+                System.out.println("personName: " + rs.getString("PATIENT_NAME"));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            logger.error(ex.getMessage()+ex.getStackTrace());
+        } finally {
+            if (con != null) try {con.close();} catch (Exception ignore){}
+        }
+        //logger.info("druid:{}",map.toString());
+        return result;
+   }
+}
