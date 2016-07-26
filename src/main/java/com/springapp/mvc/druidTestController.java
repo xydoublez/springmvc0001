5f62c2be2903dac099168efa3d6b65ef9a0752a5
李志强 <xydoublez@hotmail.com>
2016-07-26 10:25:03 +0800

    jdbcTemplate 测试成功

--- src/main/java/com/springapp/mvc/druidTestController.java
+++ src/main/java/com/springapp/mvc/druidTestController.java
@@ -14,6 +14,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016-07-20.
@@ -29,6 +30,15 @@ public class druidTestController {
         String result ="druid is very good";
         ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext("mvc-dispatcher-servlet.xml");
         DataSource dataSource = (DataSource)context.getBean("dataSource");
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        Map<String,Object> map = jdbcTemplate.queryForMap("SELECT * FROM TEST WHERE ROWNUM<2");
+        logger.info("druid:{}",map.toString());
+        return result;
+   }
+    private void test(){
+        ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext("mvc-dispatcher-servlet.xml");
+        DataSource dataSource = (DataSource)context.getBean("dataSource");
         /*
         jdbcTemplate = new JdbcTemplate(dataSource);
         Map<String,Object> map = jdbcTemplate.queryForMap("SELECT * FROM VIEW_REGLIST WHERE ROWNUM<10;");
@@ -37,7 +47,7 @@ public class druidTestController {
         try {
             con = dataSource.getConnection();
             Statement st  = con.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM VIEW_REGLIST WHERE ROWNUM<10;");
+            ResultSet rs = st.executeQuery("SELECT * FROM VIEW_REGLIST WHERE ROWNUM<10");
             while (rs.next()) {
                 System.out.println("personName: " + rs.getString("PATIENT_NAME"));
             }
@@ -48,7 +58,5 @@ public class druidTestController {
         } finally {
             if (con != null) try {con.close();} catch (Exception ignore){}
         }
-        //logger.info("druid:{}",map.toString());
-        return result;
-   }
+    }
 }
